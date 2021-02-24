5c870c613cf3851286139bc71ddd14cdfbf0463f
Louis <louis.clerc@etu.univ-grenoble-alpes.fr>
2021-02-24 21:45:31 +0100

    Add project files.

---
+++ TestApp/WebForm1.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="TestApp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <span> test 2 </span>
+
+        <br />
+
+        <asp:Label ID="Label1" runat="server" Text="test 3">test 3</asp:Label>
+
+        <br />
+
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+
+        <br />
+
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+
+        <br />
+
+        <asp:Button ID="Button1" runat="server" Text="+" OnClick="Button1_Click" />
+        <asp:Button ID="Button2" runat="server" Text="-" OnClick="Button2_Click" />
+        <asp:Button ID="Button3" runat="server" Text="*" OnClick="Button3_Click" />
+        <asp:Button ID="Button4" runat="server" Text="/" OnClick="Button4_Click" />
+
+        <br />
+
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+
+        <br />
+
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="textChange">
+            <asp:ListItem>Choix1</asp:ListItem>
+            <asp:ListItem>Choix2</asp:ListItem>
+            <asp:ListItem>Choix3</asp:ListItem>
+        </asp:DropDownList>
+
+        <br />
+
+        <asp:Label ID="Label2" runat="server">ville</asp:Label>
+
+    </form>
+</body>
+</html>
---
+++ TestApp/WebForm1.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestApp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        double a, b, c;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = "Changed";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(TextBox1.Text);
+            b = Convert.ToDouble(TextBox2.Text);
+            c = a + b;
+            TextBox3.Text = Convert.ToString(c);
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(TextBox1.Text);
+            b = Convert.ToDouble(TextBox2.Text);
+            c = a - b;
+            TextBox3.Text = Convert.ToString(c);
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(TextBox1.Text);
+            b = Convert.ToDouble(TextBox2.Text);
+            c = a * b;
+            TextBox3.Text = Convert.ToString(c);
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(TextBox1.Text);
+            b = Convert.ToDouble(TextBox2.Text);
+            c = a / b;
+            TextBox3.Text = Convert.ToString(c);
+        }
+
+        protected void textChange(object sender, EventArgs e)
+        {
+            String t = DropDownList1.Text;
+            Label2.Text = t;
+        }
+    }
+}
